b9c58c9a1d790f7b729f34c36316452ae72cc6e1
ZekromDaOne <rohan.nagashetti@gmail.com>
2023-09-28 14:31:55 -0700

    ...

--- TeamCode/src/main/java/test/code/mainJava.java
+++ TeamCode/src/main/java/test/code/mainJava.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.robotcontroller;
+package test.code;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
