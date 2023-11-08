8d647deb2725644a1aae68f00745f00611799b2d
jalev <piguyraspberry@gmail.com>
2023-11-07 19:18:17 -0800

    added double vision

--- TeamCode/src/main/java/common/Vision.java
+++ TeamCode/src/main/java/common/Vision.java
@@ -9,6 +9,8 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainCon
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 
 import java.util.List;
@@ -22,6 +24,8 @@ public class Vision {
     private static final String[] LABELS = { "Team Element" };
 
     private TfodProcessor tfod;             // TensorFlow Object Detection processor.
+
+    private AprilTagProcessor aprilTag; //AprilTag Detecction processor
     private VisionPortal visionPortal;      // Instance of the vision portal.
 
     private int gain = 16;                 // camera gain
@@ -58,6 +62,9 @@ public class Vision {
                 //.setModelInputSize(300)
                 //.setModelAspectRatio(16.0 / 9.0)
 
+                .build();
+
+        aprilTag = new AprilTagProcessor.Builder()
                 .build();
 
         // Create the vision portal by using a builder.
@@ -118,6 +125,35 @@ public class Vision {
                     recognition.estimateAngleToObject(AngleUnit.RADIANS));
         }
     }   // end method telemetryTfod()
+
+    private void telemetryAprilTag() {
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        opMode.telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                opMode.telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                opMode.telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                opMode.telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                opMode.telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                opMode.telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                opMode.telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }   // end for() loop
+
+    }   // end method telemetryAprilTag()
+
+    public void enableTensorFlow() {
+        visionPortal.setProcessorEnabled(aprilTag, false);
+        visionPortal.setProcessorEnabled(tfod, true);
+    }
+
+    public void enableAprilTag() {
+        visionPortal.setProcessorEnabled(tfod, false);
+        visionPortal.setProcessorEnabled(aprilTag, true);
+    }
 
 
     /**
