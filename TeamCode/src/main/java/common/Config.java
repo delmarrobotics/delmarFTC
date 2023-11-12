6303188a5f5280ef7e286b3d4f35b71b2df3aff7
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2023-11-11 16:14:17 -0800

    New hanging arm design

--- TeamCode/src/main/java/common/Config.java
+++ TeamCode/src/main/java/common/Config.java
@@ -11,17 +11,17 @@ public class Config {
     public static final String LEFT_BACK        = "leftRear";       // port 2
     public static final String RIGHT_BACK       = "rightRear";      // port 3
 
+    public static final String IMU             =  "imu";
     public static final String COLOR_SENSOR     = "colorSensor";
     public static final String WEBCAM           = "Webcam 1";
 
     public static final String LIFTING_WENCH    = "lifter";
     public static final String PIXEL_DROPPER    = "dropper";
 
-    public static final String HANGING_SERVO    = "hangingArmServo";
-    public static final String HANGING_MOTOR    = "hangingArmMotor";
-    public static final String HANDING_TOUCH    = "hangingArmUp";
+    public static final String HANGING_ELBOW    = "hangingElbow";
+    public static final String HANGING_WRIST    = "hangingWrist";
+    public static final String HANDING_THUMB    = "hangingThumb";
 
-    public static final String IMU             =  "imu";
     public static final String PIXEL_ARM        = "pixelArm";
     public static final String PIXEL_ELBOW      = "pixelElbow";
     public static final String HAND_UPPER       = "handUpper";
