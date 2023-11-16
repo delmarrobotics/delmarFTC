e258e114fa91cb245ec2d640b287c3bf1e0119b1
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2023-11-16 12:49:12 -0800

    ...

--- TeamCode/src/main/java/common/Config.java
+++ TeamCode/src/main/java/common/Config.java
@@ -6,14 +6,13 @@ package common;
  */
 
 public class Config {
-    public static final String LEFT_FRONT       = "leftFront";      // port 0
-    public static final String RIGHT_FRONT      = "rightFront";     // port 1
-    public static final String LEFT_BACK        = "leftRear";       // port 2
-    public static final String RIGHT_BACK       = "rightRear";      // port 3
+    public static final String LEFT_FRONT       = "leftFront";      // control hub port 0
+    public static final String RIGHT_FRONT      = "rightFront";     // control hub port 1
+    public static final String LEFT_BACK        = "leftRear";       // control hub port 2
+    public static final String RIGHT_BACK       = "rightRear";      // control hub port 3
 
     public static final String IMU             =  "imu";
     public static final String COLOR_SENSOR     = "colorSensor";
-    public static final String WEBCAM           = "Webcam 1";
 
     public static final String LIFTING_WENCH    = "lifter";
     public static final String PIXEL_DROPPER    = "dropper";
