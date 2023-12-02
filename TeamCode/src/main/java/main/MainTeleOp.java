0e8927073ac53f0518e94b71c3c6845b50f42c22
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2023-12-02 13:04:02 -0800

    Changed controls.

--- TeamCode/src/main/java/main/MainTeleOp.java
+++ TeamCode/src/main/java/main/MainTeleOp.java
@@ -37,8 +37,9 @@ public class MainTeleOp extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
-        mode = GamepadMode.PIXEL;
+        displayControls();
         robot.pixelArm.displayControls();
+        mode = GamepadMode.PIXEL;
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
@@ -49,26 +50,28 @@ public class MainTeleOp extends LinearOpMode {
             double yaw   = -gamepad1.right_stick_x / 3.0;  // Reduce rotate rate to 33%.
             double speed = gamepad1.left_trigger;
             robot.moveRobot(drive, strafe, yaw, speed);
-            //robot.moveRobot(drive, strafe, yaw);
 
             // If the start button is pressed, ignore all controls.
             if (gamepad1.start || gamepad2.start) {
                 continue;
             }
 
-            if (mode == GamepadMode.HANGING) {
-                if (robot.hangingArm.control()) {
-                    continue;
-                }
-            } else if (mode == GamepadMode.PIXEL) {
-                if (robot.pixelArm.control()) {
-                    continue;
-                }
-            }
-
             if (gamepad1.back) {
                 changeGamepadMode();
                 while (gamepad1.back) sleep(100);
+            }
+
+            if (mode == GamepadMode.HANGING) {
+                if (robot.hangingArm.control()) {
+                    continue;
+                } else if (gamepad2.a) {
+                    robot.hangRobot();
+                    while (gamepad2.a) sleep(250);
+                }
+
+            } else if (mode == GamepadMode.PIXEL) {
+                if (robot.pixelArm.control())
+                    continue;
             }
 
             if (gamepad1.left_bumper) {
@@ -78,27 +81,6 @@ public class MainTeleOp extends LinearOpMode {
                     while (gamepad1.left_trigger  > 0) {
                         sleep(100);
                     }
-
-                } else if (gamepad1.a) {
-                    hangRobot();
-
-                } else if (gamepad1.dpad_up){
-                    while (gamepad1.dpad_up){
-                        robot.lifterUp();
-                    }
-                    robot.lifterStop();
-
-                } else if (gamepad1.dpad_down) {
-                    while (gamepad1.dpad_down) {
-                        robot.lifterDown();
-                    }
-                    robot.lifterStop();
-                }
-
-            } else if (gamepad1.right_bumper) {
-                // robot lifter controls
-                if (gamepad1.a) {
-                    robot.dropPixel();
                 }
 
             } else {
@@ -110,48 +92,48 @@ public class MainTeleOp extends LinearOpMode {
                     // Toggle the intake and the two spinners on / off
                     robot.toggleIntake();
                     while (gamepad1.a) sleep(100);
+
+                } else if (gamepad1.y) {
+                    // Reverse the intake and the spinners
+                    robot.intakeReverse();
+                    while (gamepad1.y) sleep(100);
+
                 } else if (gamepad1.b) {
                     // Raise or lower the pixel intake
                     robot.toggleIntakeRotate();
                     while (gamepad1.b) sleep(100);
-                } else if (gamepad1.y){
-                    while (gamepad1.y) sleep(100);
-                } else if (gamepad1.x){
+
+                } else if (gamepad1.x) {
                     while (gamepad1.x) sleep(100);
                 }
             }
 
-            // Show the elapsed game time and wheel power.
-            //telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.update();
-        }
-    }
-
-    private void hangRobot() {
-        robot.hangingArm.wristUp();
-        sleep(2000);
-        robot.hangingArm.lockInHook();
-        robot.moveRobot(.1, 0, 0);
-        sleep(500);
-        robot.stopRobot();
-        robot.hangingArm.thumbOpen();
-        robot.hangingArm.elbowRelease();
-        sleep(500);
-        robot.hangingArm.thumbClose();
-        robot.hangingArm.wristDown();
-        robot.hangingArm.elbowDown();
-        while (gamepad1.a) {
-            sleep(250);
         }
     }
 
     private void changeGamepadMode () {
         if (mode == GamepadMode.PIXEL) {
             mode = GamepadMode.HANGING;
+            displayControls();
             robot.hangingArm.displayControls();
         } else if (mode == GamepadMode.HANGING) {
             mode = GamepadMode.PIXEL;
+            displayControls();
             robot.pixelArm.displayControls();
         }
     }
+
+    public void displayControls(){
+        telemetry.addLine("Driver Controls (Gamepad 1)\n" +
+                "  left stick - drive robot\n" +
+                "  right stick - rotate robot\n" +
+                "  left trigger - accelerate robot speed\n" +
+                "  back - toggle gamepad2 (pixelArm / hangingArm)\n" +
+                "  a - intake on / off\n" +
+                "  b - intake rotate up / down\n" +
+                "  y - intake reverse\n" +
+                "\n");
+    }
+
 }
