e0fd1207d0e6d7e9fa81224dbb07382b4da170ba
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2023-12-09 07:46:34 -0800

    Removed lineTo from trajectory sequence

--- TeamCode/src/main/java/main/RedLeftAuto.java
+++ TeamCode/src/main/java/main/RedLeftAuto.java
@@ -1,8 +1,12 @@
+/*
+ * This file contains a OpMode for the autonomous phase when the robot starts at
+ * the red left position.
+ *
+ */
+
 package main;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -10,28 +14,16 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
-import common.Logger;
+import common.Auto;
 import common.Robot;
 
-/*
- * This file contains a OpMode for the autonomous
- *
- */
-
-@Autonomous(name="Red Left Start", group="Main")  // ToDo change to @Autonomous when testing is complete
+@Autonomous(name="Red Left Start", group="Main")
 public class RedLeftAuto extends LinearOpMode {
 
     // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    private Robot robot = null;
-    private SampleMecanumDrive drive = null;
+    private final ElapsedTime runtime = new ElapsedTime();
 
-    private TrajectorySequence traj1;
-    private TrajectorySequence traj2;
-    private TrajectorySequence traj3;
-
-    private enum POSITION { left, center, right }
-    POSITION objectPosition;
+    Auto.POSITION objectPosition;
 
     @Override
     public void runOpMode() {
@@ -39,10 +31,45 @@ public class RedLeftAuto extends LinearOpMode {
         telemetry.update();
 
         // Initialize the robot hardware.
-        robot = new Robot(this);
+        Robot robot = new Robot(this);
         robot.init();
 
-         drive = new SampleMecanumDrive(hardwareMap);
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Auto auto = new Auto(this, robot, drive);
+
+        TrajectorySequence left1 = drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                .forward(24)
+                .strafeLeft(12)
+                .build();
+        TrajectorySequence left2 = drive.trajectorySequenceBuilder(left1.end())
+                .forward(21)
+                .turn(Math.toRadians(-90))
+                .forward(50)
+                .strafeRight(30)
+                .build();
+
+        TrajectorySequence right1 = drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                .forward(24)
+                .turn(Math.toRadians(-90))
+                .forward(12)
+                .build();
+        TrajectorySequence right2 = drive.trajectorySequenceBuilder(right1.end())
+                .back(12)
+                .strafeLeft(21)
+                .forward(50)
+                .strafeRight(30)
+                .build();
+
+        TrajectorySequence center1 = drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                .forward(27)
+                .build();
+        TrajectorySequence center2 = drive.trajectorySequenceBuilder(center1.end())
+                .forward(18)
+                .turn(Math.toRadians(-90))
+                .forward(50)
+                .strafeRight(30)
+                .build();
 
         while (! robot.vision.cameraReady())
             sleep(100);
@@ -52,78 +79,28 @@ public class RedLeftAuto extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
-        if (!robot.vision.findTeamElement(3000)) {
-            // Team element not found
-            objectPosition = POSITION.right;
-            telemetry.addData("dir", "Right");
-            Logger.message("Team element at right position");
-        } else {
-            // Team element is on the left spike mark
-            double angle = robot.vision.findTeamElementAngle();
-            objectPosition = POSITION.left;
-            if (angle < 0) {
-                telemetry.addData("dir", "Left");
-                Logger.message("Team element at left position, angle %f", angle);
-            } else {
-                // Team element is on the center spike mark
-                telemetry.addData("dir", "Middle");
-                Logger.message("Team element at center position, angle %f", angle);
-                objectPosition = POSITION.center;
-            }
+        auto.setColor(Auto.COLOR.RED);
+        objectPosition = auto.findTeamElement();
+
+        if (objectPosition == Auto.POSITION.left) {
+            drive.followTrajectorySequence(left1);
+            auto.purplePixel(1, 0);
+            drive.followTrajectorySequence(left2);
+            auto.yellowPixel();
+
+        } else if (objectPosition == Auto.POSITION.right) {
+            drive.followTrajectorySequence(right1);
+            auto.purplePixel(0, -1);
+            drive.followTrajectorySequence(right2);
+            auto.yellowPixel();
+
+        } else if (objectPosition == Auto.POSITION.center) {
+            drive.followTrajectorySequence(center1);
+            auto.purplePixel(1, 0);
+            drive.followTrajectorySequence(center2);
+            auto.yellowPixel();
         }
 
-        objectPosition = POSITION.center;
-
-        if (objectPosition == POSITION.left) {
-            traj1 = drive.trajectorySequenceBuilder(new Pose2d(-35.25, -62.5, Math.toRadians(90)))
-                    .strafeLeft(13)
-                    .lineTo(new Vector2d(-48.25, -31))
-                    .waitSeconds(0.5)
-                    .addDisplacementMarker(() ->robot.dropPurplePixel())
-                    .lineTo(new Vector2d(-48.25, -11.75))
-                    .turn(Math.toRadians(-90))
-                    .lineTo(new Vector2d( 11.75, -11.75))
-                    .turn(Math.toRadians(-90))
-                    .lineTo(new Vector2d(11.75, -29.25))
-                    .turn(Math.toRadians(90))
-                    .lineTo(new Vector2d(47.75,-29.25))
-                    .build();
-            drive.followTrajectorySequence(traj1);
-
-        } else if (objectPosition == POSITION.right) {
-            traj2 = drive.trajectorySequenceBuilder(new Pose2d(-35.25, -62.5, Math.toRadians(90)))
-                    .lineTo(new Vector2d(-35.25, -33))
-                    .turn(Math.toRadians(-90))
-                    .lineTo(new Vector2d(-30,-33))
-                    .waitSeconds(0.5)
-                    .addDisplacementMarker(() ->robot.dropPurplePixel())
-                    .lineTo(new Vector2d(47.25, -41.25))
-                    .build();
-            drive.followTrajectorySequence(traj2);
-
-        } else if (objectPosition == POSITION.center) {
-            traj3 = drive.trajectorySequenceBuilder(new Pose2d(-35.25, -62.5, Math.toRadians(90)))
-                    .lineTo(new Vector2d(-35.25, -29))
-                    .waitSeconds(2.5)
-                    .addDisplacementMarker(() -> robot.dropPurplePixel())
-                    //.lineTo(new Vector2d(-35.25, -11.75))
-                    //.turn(Math.toRadians(-90))
-                    //.lineTo(new Vector2d( 11.75, -11.75))
-                    //.turn(Math.toRadians(-90))
-                    //.lineTo(new Vector2d(11.75, -35.25))
-                    //.turn(Math.toRadians(90))
-                    //.lineTo(new Vector2d(47.75,-35.25))
-                    .build();
-            drive.followTrajectorySequence(traj3);
-        }
-
-
-
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-
-            //telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.update();
-        }
+        telemetry.addData("Run Time", runtime.toString());
     }
 }
