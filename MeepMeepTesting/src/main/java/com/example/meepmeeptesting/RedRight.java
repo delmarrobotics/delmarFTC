7038efeb0c5b2aa0ce4162e3c3563d54fdb1e3f2
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2023-12-09 15:57:37 -0800

    ...

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RedRight.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RedRight.java
@@ -12,21 +12,6 @@ public class RedRight {
 
         MeepMeep meepMeep = new MeepMeep(1200);
 
-        RoadRunnerBotEntity CenterElement = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(40, 40, Math.toRadians(180), Math.toRadians(180), 15)
-                .setDimensions(16,16)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(11.75, -62.5, Math.toRadians(90)))
-                                .forward(33.5)
-                                .waitSeconds(1)
-                                .back(6)
-                                .turn(Math.toRadians(-90))
-                                .forward(36.5)
-                                .waitSeconds(5)
-                                .build()
-                );
-
         RoadRunnerBotEntity LeftElement = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(40, 40, Math.toRadians(180), Math.toRadians(180), 15)
@@ -34,7 +19,6 @@ public class RedRight {
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(11.75, -62.5, Math.toRadians(90)))
                                 .forward(27.25)
-                                //.lineTo(new Vector2d(11.75, -35.25))
                                 .turn(Math.toRadians(-90))
                                 .back(15.75)
                                 .waitSeconds(1)
@@ -50,12 +34,27 @@ public class RedRight {
                 .setDimensions(16,16)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(11.75, -62.5, Math.toRadians(90)))
-                                .strafeRight(13)
                                 .forward(31.5)
-                                .waitSeconds(1)
-                                .back(10)
                                 .turn(Math.toRadians(-90))
-                                .forward(23.5)
+                                .forward(10)
+                                .waitSeconds(1)
+                                .forward(26.5)
+                                .strafeLeft(2)
+                                .waitSeconds(5)
+                                .build()
+                );
+
+        RoadRunnerBotEntity CenterElement = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(40, 40, Math.toRadians(180), Math.toRadians(180), 15)
+                .setDimensions(16,16)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(11.75, -62.5, Math.toRadians(90)))
+                                .forward(33.5)
+                                .waitSeconds(1)
+                                .back(6)
+                                .turn(Math.toRadians(-90))
+                                .forward(36.5)
                                 .waitSeconds(5)
                                 .build()
                 );
@@ -63,8 +62,8 @@ public class RedRight {
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(LeftElement)
-                .addEntity(CenterElement)
+                //.addEntity(LeftElement)
+                //.addEntity(CenterElement)
                 .addEntity(RightElement)
                 .start();
     }
