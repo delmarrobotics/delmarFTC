94f9c277e61e795d9899643756ab4ad7bbd59f72
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2023-12-09 07:50:18 -0800

    Debugging

--- TeamCode/src/main/java/test/code/DriveToAprilTag.java
+++ TeamCode/src/main/java/test/code/DriveToAprilTag.java
@@ -4,15 +4,14 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.vision.VisionPortal;
 
 import common.Logger;
 import common.Robot;
 import common.Vision;
-import main.RedLeftAuto;
 
 /*
  * Test code for the hanging arm
@@ -25,7 +24,7 @@ public class DriveToAprilTag extends LinearOpMode {
   private SampleMecanumDrive drive = null;
 
   private enum POSITION { left, center, right }
-  DriveToAprilTag.POSITION objectPosition = POSITION.center;
+  DriveToAprilTag.POSITION objectPosition = POSITION.right;
 
   @Override
   public void runOpMode() {
@@ -40,70 +39,62 @@ public class DriveToAprilTag extends LinearOpMode {
     while (! robot.vision.cameraReady())
       sleep(100);
     sleep(1000);
+
     telemetry.addData("Status", "Press start");
     telemetry.update();
-
 
     // Wait for the game to start (driver presses PLAY)
     waitForStart();
 
-    //robot.moveToColor(Robot.COLOR.BLUE, 1, 0, MIN_SPEED, 4000);
 
     // run until the end of the match (driver presses STOP)
-    boolean inPosition = false;
     while (opModeIsActive()) {
-      if (robot.vision.findAprilTag(Vision.BLUE_CENTER_TAG)) {
-        inPosition = true;
-        telemetry.update();
-        break;
-      } else {
-        Logger.message("no tag found");
-        telemetry.update();
-        break;
-      }
-    }
+      if (robot.vision.findAprilTag(Vision.BLUE_RIGHT_TAG)) { //ToDo change to left
+        double x = robot.vision.aprilTagX();
+        double range = robot.vision.aprilTagY();
+        double yaw = robot.vision.aprilTagYaw();
+        Logger.message("x %f  range %f  yaw %f", x, range, yaw);
 
-    if (inPosition) {
-      double x = robot.vision.aprilTagX();
-      Logger.message("x %f", x);
-      double range = robot.vision.aprilTagY();
-      Logger.message("range %f", range-4);
-
-      robot.moveToColor(Robot.COLOR.RED, 1, 0, 0.2,3000);
-      /*
-      Trajectory traj1 = drive.trajectoryBuilder(new Pose2d())
-              .forward(range)
-              .build();
-      drive.followTrajectory(traj1);
-
-       */
-
-      double strafe;
-      Trajectory traj2;
-      if (objectPosition == POSITION.left) {
-        traj2 = drive.trajectoryBuilder(new Pose2d())
-                .strafeLeft(5)
+        TrajectorySequence traj1 = drive.trajectorySequenceBuilder(new Pose2d())
+                .turn(Math.toRadians(yaw))
+                //.forward(range-2)
                 .build();
-        Logger.message("left");
+        drive.followTrajectorySequence(traj1);
 
-      } else if (objectPosition == POSITION.center) {
-        if (x > 0) {
+        Logger.message("robot orientation %3.1f", robot.getOrientation());
+
+        //robot.moveToColor(Robot.COLOR.RED, 1, 0, 0.2,3000);
+        //robot.moveToColor(Robot.COLOR.BLUE, 1, 0, MIN_SPEED, 4000);
+
+        double strafe = 0;
+        if (objectPosition == POSITION.left) {
+          strafe = x - 6;
+          Logger.message("left, strafe %f", strafe);
+        } else if (objectPosition == POSITION.center) {
+          strafe = x;
+          Logger.message("center, strafe %f", strafe);
+        } else {
+          strafe = 6 + x;
+          Logger.message("right, strafe %f", strafe);
+        }
+
+        Trajectory traj2;
+        if (strafe > 0) {
           traj2 = drive.trajectoryBuilder(new Pose2d())
-                  .strafeLeft(x)
+                  .strafeRight(x)
                   .build();
           drive.followTrajectory(traj2);
         }
-        else if (x < 0 ) {
+        else if (strafe < 0 ) {
           traj2 = drive.trajectoryBuilder(new Pose2d())
-                  .strafeRight(-x)
+                  .strafeLeft(-strafe)
                   .build();
           drive.followTrajectory(traj2);
         }
-        Logger.message("center");
+        break;
 
       } else {
-
-        Logger.message("right");
+        Logger.message("Tag not found");
       }
     }
   }
