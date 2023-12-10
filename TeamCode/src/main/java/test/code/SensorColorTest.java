4400a756b5b3cd95c9d05676a5b43ec04de7cb0e
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2023-12-10 12:45:15 -0800

    Move to code folder

--- TeamCode/src/main/java/test/code/SensorColorTest.java
+++ TeamCode/src/main/java/test/code/SensorColorTest.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package test.examples;
+package test.code;
 
 import android.app.Activity;
 import android.graphics.Color;
