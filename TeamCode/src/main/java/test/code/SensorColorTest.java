1c6cc5c14650145df238cf8dce253a206a536b59
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2023-12-14 10:43:25 -0800

    Renames

--- TeamCode/src/main/java/test/code/SensorColorTest.java
+++ TeamCode/src/main/java/test/code/SensorColorTest.java
@@ -66,7 +66,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
-@TeleOp(name = "Sensor: Color Test", group = "Test")
+@TeleOp(name = "Color Sensor Test", group = "Test")
 
 public class  SensorColorTest extends LinearOpMode {
 
