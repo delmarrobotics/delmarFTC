a0c9792900b064a0a9957230d86228dbd1b1b039
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2023-12-18 19:55:39 -0800

    Added calling method to output

--- TeamCode/src/main/java/common/Logger.java
+++ TeamCode/src/main/java/common/Logger.java
@@ -9,17 +9,32 @@ import android.util.Log;
 public final class Logger {
     private static final String TAG = "DELMAR";
 
+    public static String getCaller () {
+        String caller = Thread.currentThread().getStackTrace()[4].getMethodName();
+        return String.format("%-24s", caller);
+    }
+
+    private static void logString (String str) {
+        String caller = Thread.currentThread().getStackTrace()[4].getMethodName();
+        Log.d(TAG, String.format("%-24s %s", caller, str));
+    }
+
     public static void error (Exception e, String msg) {
         Log.println(Log.ERROR, TAG, msg);
         Log.e(TAG, e.getMessage());
     }
 
+    public static void warning(String warning) {
+        String caller = Thread.currentThread().getStackTrace()[4].getMethodName();
+        Log.w(TAG, String.format("%-24s %s", caller, warning));
+    }
+
     public static void message(String msg){
-        Log.d(TAG, msg);
+        logString(msg);
     }
 
     public static void message( String format, Object... args){
-        Log.d(TAG, String.format(format, args));
+        logString(String.format(format, args));
     }
 
     public static void addLine(String msg) {
