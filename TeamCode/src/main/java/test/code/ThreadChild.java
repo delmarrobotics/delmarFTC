6b26f2add09d3eebd0dccbf1afa1c2c7b5ba642d
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2023-12-19 07:55:39 -0800

    Initial version

---
+++ TeamCode/src/main/java/test/code/ThreadChild.java
@@ -0,0 +1,66 @@
+/*
+ *
+ */
+package test.code;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import common.Logger;
+import common.Process;
+
+public class ThreadChild extends Thread {
+
+    LinearOpMode opMode;
+    private final ElapsedTime runtime = new ElapsedTime();
+    boolean running = true;
+
+    public ThreadChild(LinearOpMode opMode) {
+        this.opMode = opMode;
+    }
+
+    private void sleepFor(long milliseconds){
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void end() {
+        Logger.message("stopping thread");
+        Thread.currentThread().interrupted();
+        running = false;
+    }
+
+    public void run() {
+        while (running && opMode.opModeIsActive()) {
+
+            opMode.telemetry.addData("Child", "time: %8.0f", runtime.seconds());
+            opMode.telemetry.update();
+            sleepFor(500);
+        }
+    }
+}
+
+/*
+public class ThreadChild extends Process {
+
+    LinearOpMode opMode;
+    private final ElapsedTime runtime = new ElapsedTime();
+
+    public ThreadChild(LinearOpMode opMode) {
+        super();
+        this.opMode = opMode;
+    }
+
+    @Override
+    public void run() {
+        while (running && opMode.opModeIsActive()) {
+
+            opMode.telemetry.addData("Child", "time: %8.0f", runtime.seconds());
+            opMode.telemetry.update();
+            sleep(500);
+        }
+    }
+*/
---
+++ TeamCode/src/main/java/test/code/ThreadParent.java
@@ -0,0 +1,50 @@
+package test.code;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.stream.CameraStreamSource;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+/*
+ * Test code for the hanging arm
+ */
+@TeleOp(name="Thread Test", group="Test")
+@SuppressWarnings("unused")
+public class ThreadParent extends LinearOpMode {
+
+  private final ElapsedTime runtime = new ElapsedTime();
+  //Logger logger = Logger.getLogger("MyLog");
+  ThreadChild child  = null;
+
+  @Override
+  public void runOpMode() {
+      telemetry.addData("Status", "Press start");
+      telemetry.update();
+      Servo servo;
+
+      ThreadChild child = new ThreadChild(this);
+
+      waitForStart();
+
+      new Thread(child).start();
+      runtime.reset();
+
+      while (opModeIsActive()) {
+
+        if (gamepad1.a)
+            servo = hardwareMap.get(Servo.class, "zzz");
+        if (gamepad1.x)
+          child.end();
+
+        telemetry.addData("Parent", "time: %8.0f", runtime.seconds());
+        telemetry.update();
+        sleep(2000);
+      }
+  }
+}
