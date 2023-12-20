93c89f610075bc23c77f462f2f8bddce87b241ad
Piguyraspberry <136662119+Piguyraspberry@users.noreply.github.com>
2023-12-20 13:53:20 -0800

    Moved RickPlayer.java

--- TeamCode/src/main/java/common/RickPlayer.java
+++ TeamCode/src/main/java/common/RickPlayer.java
@@ -1,13 +1,26 @@
+package common;
+
 import android.media.MediaPlayer;
+
 public class RickPlayer {
+
+  public LinearOpMode opMode;
+
   //The player handling the audio
-  private static MediaPlayer mediaPlayer = null;
+  private static MediaPlayer mediaPlayer;
+
+  //constructor
+  public RickPlayer(LinearOpMode opMode) {
+    this.opMode = opMode;
+  }
+
   //Start the wubs
   public static void start(Context context) {
     if (mediaPlayer == null) mediaPlayer = MediaPlayer.create(context, R.raw.rick);
     mediaPlayer.seekTo(0);
     mediaPlayer.start();
   }
+  
   //Stop the wubs
   public static void stop() {
     if (mediaPlayer != null) {
@@ -16,6 +29,4 @@ public class RickPlayer {
       catch (IOException e) {}
     }
   }
-}
-public class SoundOpMode extends OpMode {
 }
