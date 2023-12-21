9506b9ce3230cff9a1207b8473226d87d3080f8b
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2023-12-20 18:15:38 -0800

    Test joystick driving now on a separate thread.

--- TeamCode/src/main/java/test/code/ThreadParent.java
+++ TeamCode/src/main/java/test/code/ThreadParent.java
@@ -1,6 +1,7 @@
 package test.code;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -10,6 +11,8 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.stream.CameraStreamSource;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvWebcam;
+
+import common.Drive;
 
 /*
  * Test code for the hanging arm
@@ -24,15 +27,22 @@ public class ThreadParent extends LinearOpMode {
 
   @Override
   public void runOpMode() {
+      telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
       telemetry.addData("Status", "Press start");
       telemetry.update();
       Servo servo;
 
-      ThreadChild child = new ThreadChild(this);
+      //ThreadChild child = new ThreadChild(this);
+      //child.start();
+
+      Drive drive = new Drive(this);
+      drive.start();
 
       waitForStart();
 
-      new Thread(child).start();
+
+      //new Thread(child).start();
       runtime.reset();
 
       while (opModeIsActive()) {
