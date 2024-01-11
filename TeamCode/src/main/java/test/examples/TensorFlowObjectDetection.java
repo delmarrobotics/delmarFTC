ac343e9136205211ab9d177f284f1ebb5ad87f46
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2024-01-11 12:19:34 -0800

    New TensorFlow model

--- TeamCode/src/main/java/test/examples/TensorFlowObjectDetection.java
+++ TeamCode/src/main/java/test/examples/TensorFlowObjectDetection.java
@@ -64,7 +64,7 @@ public class TensorFlowObjectDetection extends LinearOpMode {
 
     // TFOD_MODEL_ASSET points to a model file stored in the project Asset location,
     // this is only used for Android Studio when using models in Assets.
-    private static final String TFOD_MODEL_ASSET = "MyModelStoredAsAsset.tflite";
+    private static final String TFOD_MODEL_ASSET = "BlueRed.tflite";
     // TFOD_MODEL_FILE points to a model file stored onboard the Robot Controller's storage,
     // this is used when uploading models directly to the RC using the model upload interface.
     //private static final String TFOD_MODEL_FILE = "team_prop_1.tflite";
@@ -149,8 +149,8 @@ public class TensorFlowObjectDetection extends LinearOpMode {
             // choose one of the following:
             //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
             //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
-            //.setModelAssetName(TFOD_MODEL_ASSET)
-            .setModelFileName(TFOD_MODEL_FILE)
+            .setModelAssetName(TFOD_MODEL_ASSET)
+            //.setModelFileName(TFOD_MODEL_FILE)
 
             // The following default settings are available to un-comment and edit as needed to
             // set parameters for custom models.
