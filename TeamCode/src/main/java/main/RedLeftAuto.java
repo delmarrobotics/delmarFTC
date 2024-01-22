3dac2368c07aa9012834351e3b655ad54031157a
MaverickTopG <iris.teyssier@gmail.com>
2024-01-22 08:59:04 -0800

    WINNING CODE!!!

--- TeamCode/src/main/java/main/RedLeftAuto.java
+++ TeamCode/src/main/java/main/RedLeftAuto.java
@@ -8,6 +8,7 @@ package main;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import common.Auto;
