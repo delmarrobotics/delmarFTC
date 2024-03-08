aa5834ded6f5678fb4479522e14c36dc11fca410
MaverickTopG <iris.teyssier@gmail.com>
2024-03-08 12:26:21 -0800

    commit

---
+++ TeamCode/src/main/java/main/MainTeleOp.java
@@ -0,0 +1,17 @@
+package main;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import common.Config;
+
+
+public class MainTeleOp extends OpMode {
+
+    public void init() {
+        String camera = Config.CAMERA;
+    }
+
+    public void loop() {
+
+    }
+}
